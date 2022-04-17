6d4725ee73f836b8826bf609740ad131a227f08e
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-04-18 04:12:26 +0600

    done 404 notFound

--- src/Pages/Shared/NotFound/NotFound404.js
+++ src/Pages/Shared/NotFound/NotFound404.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const NotFound404 = () => {
-    return (
-        <div>
-            this is not found
-        </div>
-    );
+  return (
+    <div className="mt-5 text-center">
+      <img
+        src="https://cdn1.vectorstock.com/i/1000x1000/77/15/404-error-page-not-found-plug-graphic-background-vector-21467715.jpg"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default NotFound404;
